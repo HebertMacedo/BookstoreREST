3e992fdd65f01f75a64b6a2ac4209fa78cfeedf2
Hebert Rodrigues <nayrasantos@Nayras-MacBook-Pro.local>
2023-12-29 22:06:15 -0300

    try catch

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,8 +2,14 @@ import book from "../models/Book.js";
 
 class BookController {
     static async listBooks (req, res){
+        try {
         const listBooks = await book.find({});
         res.status(200).json(listBooks);
+        
+        } catch (erro) {
+
+        }
+        
     };
 
     static async registerBook (req, res) {
